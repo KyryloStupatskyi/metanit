1f4c9f9ba548ab85be621409ac1542547116d11c
KyryloStupatskyi <stupackijk@gmail.com>
2024-03-18 22:45:26 +0100

    send a response

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
 const express = require("express")
-const fs = require("fs")
 require("./consts")
 
 const app = express()
 
-app.use((req, res, next) => {
-  const now = new Date()
-  const hour = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-  const data = `${hour}:${minutes}:${seconds} ${req.method} ${req.url}`;
+// app.use((req, res) => {
+//   res.sendFile(__dirname + '/index.html')
+// })
 
-  console.log(data)
-
-  fs.appendFile('server.log', data + '\n', (err) => {
-    if (err) {
-      console.log(err)
-    }
-  })
-  next()
+app.use("/home/some", (req, res) => {
+  res.status(404).send("Resourses not found")
 })
 
-app.get("/", (req, res) => {
-  res.send("Home Page")
-})
-
-
-app.listen(globalThis.PORT, () => console.log(`Server is wokring at adress http://localhost:${globalThis.PORT}`))
+app.listen(globalThis.PORT, () => console.log(`Server is Working at adress http://localhost:${globalThis.PORT}`))
