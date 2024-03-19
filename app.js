250432c5a623ad2284b58bddafd168c89d87212d
Kyrylo <stupackijk@gmail.com>
2024-03-19 16:34:22 +0100

    create redirect pages

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ const app = express()
 
 app.use("/static", express.static("public"))
 
-app.use('/', (req, res) => {
-  res.send("<h1>Main Page</h1>")
+// app.use("/home", (req, res) => {
+//   res.redirect("https://metanit.com")
+// })
+
+app.get("/someRoute", (req, res) => {
+  res.redirect("about")
+})
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Page</h1>")
+})
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html")
 })
 
 app.listen(globalThis.PORT, () => console.log(`Server is Working at adress http://localhost:${globalThis.PORT}`))
